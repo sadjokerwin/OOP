60744c123a98865f4cda54469ae299fc3fdb34b3
sadjokerwin <al107ivanov@gmail.com>
2022-04-27 17:44:50 +0300

    Date.update 1
    Added more functionality with class Date

--- Project 1/Date.cpp
+++ Project 1/Date.cpp
@@ -1,5 +1,51 @@
 #include <iostream>
 #include "Date.h"
+void enterDate()
+{
+    int day, month, year;
+    while (!isValidDate(day, month, year))
+    {
+        cin >> day >> month >> year;
+    }
+    Date(day, month, year);
+}
+bool isValidDate(int day, int month, int year)
+{
+    if (month > 0 && month < 13)
+    {
+        if (month == 1 && month == 3 && month == 5 && month == 7 && month == 8 && month == 10 && month == 12)
+        {
+            return day == 31;
+        }
+        else if (month == 2)
+        {
+            if (isLeapYear(year))
+                return day == 29;
+            else
+                return day == 28;
+        }
+        else
+            return day == 30;
+    }
+}
+bool isLeapYear(int year)
+{
+    if (year <= 1916)
+        return year % 4 == 0;
+    return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+}
+Date::Date()
+{
+    mDay = 1;
+    mMonth = 1;
+    mYear = 1;
+}
+Date::Date(size_t day, size_t month, size_t year)
+{
+    setDay(day);
+    setMonth(month);
+    setYear(year);
+}
 void Date::setDay(int day)
 {
     mDay = day;
@@ -10,6 +56,11 @@ void Date::setMonth(int month)
 }
 void Date::setYear(int year)
 {
+    this->mYear = year;
+    if (isLeapYear(year))
+        monthDays[1] = 29;
+    else
+        monthDays[1] = 28;
     mYear = year;
 }
 size_t Date::getDay() const
@@ -23,4 +74,70 @@ size_t Date::getMonth() const
 size_t Date::getYear() const
 {
     return mYear;
+}
+int operator-(const Date &lhs, const Date &rhs)
+{
+    int lhsDayNum, rhsDayNum, diff;
+    for (int i = 0; i < lhs.mMonth - 1; i++)
+    {
+        lhsDayNum += lhs.monthDays[i];
+
+    }
+    lhsDayNum += lhs.mDay;
+    for (int i = 0; i < rhs.mMonth - 1; i++)
+    {
+        rhsDayNum += rhs.monthDays[i];
+
+    }
+    rhsDayNum += rhs.mDay;
+    diff = rhsDayNum - lhsDayNum;
+    if (diff < 0)
+        // throw "The first date is a later one than the second date!";
+        return -1;
+    else
+        return (diff);
+    // if (isValidDate(mDay, mMonth, mYear))
+    // {
+    //     return *this;
+    // }
+    // else
+    // {
+    //     if ((mMonth - 1) >= 1)
+    //     {
+    //         mMonth -= 1;
+    //         if (mMonth == 1 && mMonth == 3 && mMonth == 5 && mMonth == 7 && mMonth == 8 && mMonth == 10 && mMonth == 12)
+    //         {
+    //             mDay = 31;
+    //         }
+    //     }
+    // }
+}
+// Date Date::operator+=(const Date &other)
+// {
+//     mDay += other.mDay;
+//     for (int i = 1; i <= other.mDay; i++)
+//     {
+//         if (isValidDate(mDay, mMonth, mYear))
+//         {
+//             return *this;
+//         }
+//         else
+//         {
+//             if ((mMonth + 1) <= 12)
+//             {
+//                 mMonth += 1;
+//                 mDay = 1;
+//             }
+//             else
+//             {
+//                 mYear += 1;
+//                 mMonth = 1;
+//                 mDay = 1;
+//             }
+//         }
+//     }
+// }
+std::ostream &operator<<(std::ostream &out, const Date &date)
+{
+    return out << date.mDay << date.mMonth << date.mYear;
 }
--- Project 1/Date.h
+++ Project 1/Date.h
@@ -2,22 +2,28 @@
 #define __DATE_H
 #include <iostream>
 using namespace std;
+void enterDate();
+bool isValidDate(int, int, int);
+bool isLeapYear(int);
 class Date
 {
 private:
     size_t mDay;
     size_t mMonth;
     size_t mYear;
+    int monthDays[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
 public:
     Date();
-    Date(size_t, size_t, size_t)
-    void setDay(int day);
-    void setMonth(int month);
-    void setYear(int year);
+    Date(size_t, size_t, size_t);
+    void setDay(int);
+    void setMonth(int);
+    void setYear(int);
     size_t getDay() const;
     size_t getMonth() const;
     size_t getYear() const;
+    friend int operator-(const Date &lhs, const Date &rhs);
+    // Date &operator+=(const Date &other);
+    friend std::ostream &operator<<(std::ostream &, const Date &);
 };
 #endif
-#kur
